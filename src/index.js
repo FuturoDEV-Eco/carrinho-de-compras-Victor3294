1429ea5ab9cbb9430d951d190e8099bfdeb4c6f8
Victor3294 <vblum26@gmail.com>
2024-06-30 20:56:16 -0300

    feat: 🎸 Resolução do exercicio 3
    Além da resolução do exercicio 3, foi criado o arquivo que contem todos
    as querys usadas no banco de dados

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const clientsRouter = require("./routes/clients.routes")
+const productsRouter = require("./routes/products.routes")
 
 const app = express()
 app.use(express.json())
 app.use('/clients', clientsRouter)
+app.use('/products', productsRouter)
 
 app.listen(3000, () => {
     console.log("Servidor Online")
